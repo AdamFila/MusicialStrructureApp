fb2440a9fdcbd668ba17243ced416ad64533b09b
Adam Fila <af8tab@gmail.com>
2018-04-14 13:16:29 +0200

    Again Update MainActivity, and Add Library, NowPlaying Activities

--- app/src/main/java/com/example/android/musicialstrructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicialstrructureapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicialstrructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button library = (Button) findViewById(R.id.library);
+        Button nowPlaying = (Button) findViewById(R.id.now_playing);
+
+        library.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, LibraryActivity.class);
+
+                startActivity(numbersIntent);
+            }
+        });
+
+        nowPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+
+                startActivity(numbersIntent);
+            }
+        });
     }
 }
